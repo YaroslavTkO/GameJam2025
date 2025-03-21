a99c077fb9ad77c2db43ae237eec1fece24e367b
YaroslavTk <yaroscro@gmail.com>
2025-03-21 22:58:44 +0200

    added decorations generation

---
+++ Assets/Scripts/Generation/DecorationGenerator.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorationGenerator : MonoBehaviour
+{
+    public GameObject[] DecorationPrefabs;
+    public Transform train;
+    public List<GameObject> decorationsList;
+    private float spawnPosition = 0f;
+    public float decorationSpace;
+    public int decorationCount;
+
+    void SpawnDecoration()
+    {
+        float x = Random.Range(1, 5);
+        GameObject DecorationRight = Instantiate(
+            DecorationPrefabs[Random.Range(0, DecorationPrefabs.Length)], new Vector3(Random.Range(1, 5), spawnPosition), Quaternion.identity);
+        decorationsList.Add(DecorationRight);
+        GameObject DecorationLeft = Instantiate(
+            DecorationPrefabs[Random.Range(0, DecorationPrefabs.Length)], new Vector3(-Random.Range(1, 5), spawnPosition + Random.Range(0f, 1f)), Quaternion.identity);
+        decorationsList.Add(DecorationLeft);
+        spawnPosition += decorationSpace;
+    }
+
+    void RemoveOldDecoration()
+    {
+        if (decorationsList.Count > decorationCount)
+        {
+            Destroy(decorationsList[0]);
+            decorationsList.RemoveAt(0);
+        }
+    }
+
+    void Update()
+    {
+        if (train.position.y > spawnPosition + 3 * decorationSpace - decorationCount / 2 * decorationSpace)
+        {
+            SpawnDecoration();
+            RemoveOldDecoration();
+        }
+    }
+}
